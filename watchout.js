e5a26d8a63779eba4dedffd4672805e7157adbec
Rohan <sarith21@gmail.com>
2014-04-25 14:53:35 -0700

    beginning on pair station

--- watchout.js
+++ watchout.js
@@ -3,14 +3,23 @@
 var width = 400;
 var height = 400;
 
-var enemyCount = 10; //stores number of emenies
+var enemyData = [
+	{"cx": 20, "cy": 20, "radius": 20, "color": "blue"}];
 
 
-
+//our primary svg element. everything will go inside this
 var svg = d3.select("body").append("svg") //create svg element
 	.attr("width", width) //set svg width using css attr()
 	.attr("height", height); //set svg height using css attr()
 
-//group all nodes inside svg element (for reference later)
+//add a group to hold all enemies
 var enemyGroup = svg.append("g");
 
+//add enemies to enemy group
+var enemies = enemyGroup.selectAll("enemy")
+	.data(enemyData)
+	.enter()
+	.append("enemy");
+
+//add enemy attributes as a property of enemies, defined above
+var enemyAttributes = enemies.
