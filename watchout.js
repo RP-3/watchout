3d57339ad4455633ae79677547acfcbfc5179e43
Rohan <sarith21@gmail.com>
2014-04-25 13:04:12 -0700

    created svg element with basic dimension properties

--- watchout.js
+++ watchout.js
@@ -1,76 +1,16 @@
-// start slingin' some d3 here.
 
-var alphabet = "abcdefghijklmnopqrstuvwxyx".split("");
+//will be used to set properties of the svg element
+var width = 400;
+var height = 400;
 
-var width = 960;
-var height = 500;
-
-var svg = d3.select("body").append("svg")
-	.attr("width", width)
-	.attr("height", height)
-  .append("g")
-  	.attr("transform", "translate(32," + (height/2)+ ")");
-
-var update = function(data){
-	//Join new data with old elements, if applicable
-	var text = svg.selectAll("text")
-		.data(data);
-
-	//update old elements as required
-	text.attr("class", "update");
-
-	//create new elements as needed
-	text.enter().append("text")
-		.attr("class", "enter")
-		.attr("x", function(d, i){return i*32;})
-		.attr("dy", ".35em");
-
-	//Enter and update
-	text.text(function(d){return d;});
-
-	//exit
-	text.exit().remove();
-}	
-
-//initial display
-update(alphabet);
-
-//grab random letters from alphabet, in alphabetical order
-setInterval(function() {
-update(shuffle(alphabet)
-  .slice(0, Math.floor(Math.random() * 26))
-  .sort());
-}, 1500);
+var enemyCount = 10; //stores number of emenies
 
 
 
-//extranious shuffling function. Unimportant.
-var shuffle = function(array){
-	var m = array.length;
-	var t;
-	var i;
+var svg = d3.select("body").append("svg") //create svg element
+	.attr("width", width) //set svg width using css attr()
+	.attr("height", height); //set svg height using css attr()
 
-	while(m){
-		i = Math.floor(Math.random() * m);
-		m--;
-		t = array[m];
-		array[m] = array[i];
-		array[i] = t;
-	}
-	return array;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//group all nodes inside svg element (for reference later)
+var enemyGroup = svg.append("g");
 
